0578501e81a7220d44090d54ac291076333f68aa
Hginag <phgiang1389@gmail.com>
2024-08-22 18:09:54 +0700

    add file and image

---
+++ Part1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework</title>
+    <style>
+        img {
+            object-fit: cover;
+            height: 300px;
+            width: auto;
+        }
+        h1{
+            color: lightblue;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo '<h1>Nature Photo</h1><br>';
+    echo '<center><img src = "nature.jpg" alt = "Nature Photo"></center>';
+    ?>
+</body>
+</html>
